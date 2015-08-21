0d765285f6589530a7fe6b642c6a9b57588a0859
Mal Blackwell <mal_blackwell@yahoo.com>
2015-08-20 18:20:12 -0700

    got menu working, but still wonky

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('.button').click(function() {
+    $('.content').toggleClass('off');
+  });
+  $('.topNav a').click(function() {
+    $('.content').toggleClass('off');
+  });
+});
