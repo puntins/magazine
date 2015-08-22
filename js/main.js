e596575888fff1d685c08cd90ade48dee5a254c2
Mal Blackwell <mal_blackwell@yahoo.com>
2015-08-22 15:53:27 -0700

    fixed scrolling svg for mobile

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
   $('.button').click(function() {
     $('.content').toggleClass('off');
+    $('.svgLogo').toggleClass('off');
   });
+
   $('.topNav a').click(function() {
     $('.content').toggleClass('off');
+    $('.svgLogo').toggleClass('off');
   });
 });
